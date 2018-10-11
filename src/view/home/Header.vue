48f73343dd29552c2d6d3450358f568bdbb00adb
Yoo-Youngjae <dudwo235@naver.com>
2018-10-11 23:38:32 +0900

    18/10/11 시작

---
+++ src/view/home/Header.vue
@@ -0,0 +1,41 @@
+<template>
+  <div >
+    <div class="header-wrapper">
+      <v-layout align-center row fill-height>
+        <img src="../../assets/logo.png" class="logo" >
+
+        <v-spacer></v-spacer>
+        <h4 class="color-white cp mr-3" @click="showDevelopAlert">언어변경</h4>
+      </v-layout>
+    </div>
+    <div class="dummy-header">
+
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "Header",
+        methods:{
+
+        }
+    }
+</script>
+
+<style scoped>
+  .header-wrapper{
+    height: 60px;
+    background-color: #3CD070;
+    position: fixed;
+    width: 100%;
+  }
+  .dummy-header{
+    height: 60px;
+  }
+  .logo{
+    width: 80px;
+    margin-left: 20px;
+    cursor: pointer;
+  }
+</style>
---
+++ src/view/home/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-app>
+    <my-header></my-header>
+    <div class="mainView">
+      <router-view/>
+    </div>
+    <my-footer></my-footer>
+  </v-app>
+</template>
+
+<script>
+  import MyHeader from './Header'
+  import MyFooter from './Footer'
+    export default {
+      name: "Home",
+      components:{
+        MyHeader, MyFooter
+      },
+      data: function () {
+        return {
+          counter: 0,
+        }
+      }
+    }
+</script>
+
+<style scoped>
+  .mainView{
+    min-height: 700px;
+    max-width: 1200px;
+    width: 100%;
+    margin-left: auto;
+    margin-right: auto;
+    margin-top: 64px;
+  }
+</style>
